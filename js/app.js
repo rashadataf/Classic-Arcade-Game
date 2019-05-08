3eaf2de81019e083a69267bbde9acbff9b5615ef
Rashad Attaf <rashadattaf@gmail.com>
2019-05-08 13:13:44 +0300

    feat : The Player function (Constructor)

--- js/app.js
+++ js/app.js
@@ -59,7 +59,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+let Player = function() {
+    this.x = 204;
+    this.y = 400;
+    this.sprite = 'images/char-cat-girl.png';
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
